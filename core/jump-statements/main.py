0710f863b3b5b73248a3d2da0bc7dee66828c50c
brijeshpytops <brijeshgondaliya.tops@example.com>
2024-07-25 15:55:51 +0530

    jump statments, string

---
+++ core/jump-statements/main.py
@@ -0,0 +1,13 @@
+nums = 10
+
+for num in range(1, nums+1):
+
+    if num == 3:
+        continue
+
+    if num == 5:
+        pass
+
+    if num == 7:
+        break
+    print(num)
---
+++ core/string/main.py
@@ -0,0 +1,99 @@
+"""
+String: immutable
+
+What is string?
+
+A string is a sequence of characters. In Python, a string is a collection of characters enclosed in single quotes (') or double quotes (").
+
+sytnax :
+
+string_name = "value"
+"""
+
+# Sample code to demonstrate string declaration
+
+my_string = "Hello, World!"
+my_string = 'Hello, World!'
+my_string = '''Hello, World!'''
+my_string = """Hello, World!"""
+
+# print(type(my_string))
+
+code = "python"
+# print(len(code))
+
+# print(code)
+
+# indexing (+/-)
+# p 0 -6
+# y 1 -5
+# t 2 -4
+# h 3 -3
+# o 4 -2
+# n 5 -1
+
+# print(code[-4])
+# print(code[2])
+
+# slicing (+/-)
+# print(code[2:5])
+# print(code[-2:-5:-1][::-1])
+
+# print(code[-4:-1]) # positive
+
+
+# concat:
+
+# fname = "brijesh"
+# lname = "gondaliya"
+
+# print(fname + " " + lname)
+
+# replica
+
+# print(code * 3)
+
+# for ch in code:
+#     print(chr(ord(ch)))
+
+# string methods
+# print(dir(code))
+
+name = "TopS TEchNolOGy PvT. LTd."
+# print(name.lower())
+# print(name.upper())
+# print(name.capitalize())
+# print(name.title())
+# print(name.swapcase())
+
+# space = "     python code    "
+# print(space.rstrip())
+# print(space.lstrip())
+# print(space.strip())
+# print(space.replace(" ", ""))
+
+# print("TopS TEchNolOGy PvT. LTd.".lower().find("Tech".lower()))
+
+# print("TopS TEchNolOgy PvT. LTd.".lower().startswith("tops"))
+
+# print("TopS TEchNolOgy PvT. LTd.".lower().endswith(".ltd."))
+
+center = "python"
+# print(center.center(20, '*'))
+
+# print('name : "brijesh gondaliya"')
+# print("name : 'brijesh gondaliya'")
+# print("name :\"brijesh gondaliy\"")
+# print('\'')
+
+name = "python"
+price = 399.256765367450
+pages = 345
+
+# print("my book name is python and its price and page is 399.20 and 345.")
+# print(f"my book name is {name} and its price and page is {price} and {pages}.")
+# print("my book name is {} and its price and page is {} and {}.".format(name, price, pages))
+# print("my book name is {0} and its price and page is {1} and {2}.".format(name, price, pages))
+# print("my book name is {1} and its price and page is {2} and {3}.".format(name,name, price, pages))
+print("my book name is %s and its price and page is %.2f and %d." % (name, price, pages))
+
