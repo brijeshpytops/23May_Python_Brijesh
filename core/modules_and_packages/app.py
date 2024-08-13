d09592af5395a44fb292a15572dbdfe93bd072fb
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-08-13 15:38:17 +0530

    modules and packages

---
+++ core/modules_and_packages/app.py
@@ -0,0 +1,43 @@
+import math
+
+# print(dir(math))
+# print(math.factorial(5))
+# print(math.sqrt(25))
+
+import random
+
+# print(random.random())
+# print(random.randint(111111,999999))
+# print(random.randrange(1, 10, 2))
+# musics = ['music-1.mp3','music-2.mp3','music-3.mp3','music-4.mp3','music-5.mp3',]
+
+# random.shuffle(musics)
+# print(musics)
+
+
+import os
+# print(os.curdir)
+# print(os.listdir('musics'))
+
+# import pygame
+
+# # Initialize pygame mixer
+# pygame.mixer.init()
+
+# for music in os.listdir('musics'):
+#     pygame.mixer.music.load(r"C:\Brijesh_Work\batch\23May_Python_Brijesh\core\modules_and_packages\musics" + '\\' + f"{music}")
+
+#     # Play the MP3 file
+#     pygame.mixer.music.play()
+
+#     # Keep the program running while the music is playing
+#     while pygame.mixer.music.get_busy():
+#         pygame.time.Clock().tick(10)
+
+
+import uuid
+
+# print(uuid.uuid4())
+
+# os.rename('26628489-e2da-4fbc-a371-0cf4d8153d93.png', f"{str(uuid.uuid4())}_IG0001.png")
+os.system('py main1.py')
---
+++ core/modules_and_packages/main1.py
@@ -0,0 +1,1 @@
+print("This is a main1.py file")
